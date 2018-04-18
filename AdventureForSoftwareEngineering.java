7f601ecea14087c5d6c021834b85375d30d0f5d0
Ameer <abaksh@afsenyc.org>
2018-04-18 11:54:35 -0400

    finished starbucks class

--- AdventureForSoftwareEngineering.java
+++ AdventureForSoftwareEngineering.java
@@ -12,9 +12,9 @@ public class AdventureForSoftwareEngineering implements Game{
     @Override
     public List<Location> getLocations(){
         List<Location> levels = new ArrayList<Location>();
-        levels.add(new HouseWakeUp());
+        //levels.add(new HouseWakeUp());
         levels.add(new Starbucks());
-        levels.add(new AXSE());
+        //levels.add(new AXSE());
         
         return levels;
     }
--- Starbucks.java
+++ Starbucks.java
@@ -14,7 +14,23 @@ public class Starbucks implements Location
       p.changeHour(1);
       p.changeTiredness(1);
       Thread.sleep(1000);
-      System.out.println("Do you want to get the regular or try something new?  [Reg] or [New]");
+      regOrNew();
+      return "AXSE";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public String getName()
+    {
+        return "Starbucks";
+    }
+    
+    public void regOrNew() throws InterruptedException{
+       System.out.println("Do you want to get the regular or try something new?  [Reg] or [New]");
       Scanner sc = new Scanner(System.in);
       String userinput = sc.next();
       userinput = userinput.toUpperCase();
@@ -30,51 +46,62 @@ public class Starbucks implements Location
         Thread.sleep(1000);
         System.out.println("Is that all? -Ashley asks");
         Thread.sleep(1000);
-        yesOrNo();
-      }
-      //else if()
-      return "AXSE";
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public String getName()
-    {
-        return "Starbucks";
-    }
-    
-    public void yesOrNo(){
-      System.out.println("[Yes] or [No]?");
-      //Thread.sleep(1000);
-      Scanner scc = new Scanner(System.in);
-      String input = scc.next();
-      input = input.toUpperCase();
-       if(input.equals("YES")){
-        System.out.println("Of course, you always get the same thing every day");
-        //Thread.sleep(1000);         
-      }
-       else if(input.equals("NO")){
-          System.out.println("What else would you like?");
-          //Thread.sleep(1000);
-          Scanner sccc = new Scanner(System.in);
-          String rawr = sccc.nextLine();
-          System.out.println(".");
-          //Thread.sleep(100);
-          System.out.println(".");
-          //Thread.sleep(100);
-          System.out.println(".");
-          //Thread.sleep(1000);
-          System.out.println("Ok, here's your " + rawr + "!");
+        int temp = 0;
+        while(temp == 0){
+          System.out.println("[Yes] or [No]?");
+          Thread.sleep(1000);
+          Scanner scc = new Scanner(System.in);
+          String input = scc.next();
+          input = input.toUpperCase();
+           if(input.equals("YES")){
+            System.out.println("Of course, you always get the same thing every day.");
+            Thread.sleep(1000);
+            temp++;
+          }
+           else if(input.equals("NO")){
+              System.out.println("What else would you like?");
+              Thread.sleep(1000);
+              Scanner sccc = new Scanner(System.in);
+              String rawr = sccc.nextLine();
+              System.out.println(".");
+              Thread.sleep(100);
+              System.out.println(".");
+              Thread.sleep(100);
+              System.out.println(".");
+              Thread.sleep(1000);
+              System.out.println("Ok, here's your " + rawr + "! -Ashley hand you your " + rawr);
+              temp++;
+            }
+            else{
+              System.out.println("Excuse me?");
+              Thread.sleep(1000);
+          }
         }
-        else{
-          System.out.println("Excuse me?");
-          //Thread.sleep(1000);
       }
+      else if(userinput.equals("NEW")){
+         System.out.println("Really?");
+         Thread.sleep(1000);
+         System.out.println("What do you want to try out today? -Ashley asks!");
+         Thread.sleep(1000);
+         Scanner scan = new Scanner(System.in);
+         String inputt = scan.nextLine();
+         inputt = inputt.toLowerCase();
+         Thread.sleep(1000);
+         System.out.println("Alright, one " + inputt + " coming right up! -Ashley asks!");
+         System.out.println(".");
+         Thread.sleep(100);
+         System.out.println(".");
+         Thread.sleep(100);
+         System.out.println(".");
+         Thread.sleep(1000);
+         System.out.println("Alright here you go! -Ashley hand you your " + inputt);
+      }
+      else{
+         System.out.println("Excuse me?");
+         Thread.sleep(1000);
+         regOrNew();
       }
     }
+  }
    
 
