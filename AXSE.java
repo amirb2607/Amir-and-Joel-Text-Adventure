bae0e9a65691f93f27abbc08e84868cea4dc968c
joelm5793 <joelm5793@afsenyc.org>
2018-04-18 11:57:37 -0400

    Add files via upload

--- AXSE.java
+++ AXSE.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class WalkToAFSE here.
  *
@@ -7,27 +7,35 @@
  */
 public class AXSE implements Location
 {
-    private int timeHr = 4;
-    private String month = "March";
-    private int day = 8;
-
+    private int date ;
+    private int gpa = 76;
     @Override
     public String getName(){
         // Location name will match class name for convenience
         return "WalkToAXSE";
     }
-
-        @Override
-        public String enter(Player p)
-    {
-            String nextLocationName;
-                if(timeHr >= 9){
-                System.out.println("Finally here in AXSE *Sigh");
-                
-            
-            
-           }
-           return "HouseWakeUp";
+          @Override
+          public String enter(Player p)  throws InterruptedException{
+          {
+                   String nextLocationName; 
+                   System.out.println("Finally here in AXSE *Sigh");
+                   Thread.sleep(1000);
+                   System.out.println("what time is it? " + p.getHour());
+                   Thread.sleep(1000);
+                   System.out.println("Hey Player are you going to class?");
+                   Thread.sleep(1000);
+                   System.out.println("Do you want to go to class? [Yes] or [No]");
+                   Thread.sleep(1000);
+                   Scanner scc = new Scanner(System.in);
+                   String input = scc.next();
+                   input = input.toUpperCase();
+                    if(input.equals("NO")){
+                       System.out.println("Um no I'm gonna leave right now.");
+                       Thread.sleep(1000);
+                       p.changeHour(p.getHour() - 1);
+                   }
+                   return "HouseWakeUp";
+                } 
+         
     }
 }
-
