7b49dbd9eb82cf280297de2309db2f93aaec4537
AFSECSCapstoneStudent <afsecscapstonestudent@librarya101a263.78m460.nycboe.org>
2018-04-23 09:39:37 -0400

    Updated Starbucks

--- AXSE.java
+++ AXSE.java
@@ -12,30 +12,29 @@ public class AXSE implements Location
     @Override
     public String getName(){
         // Location name will match class name for convenience
-        return "WalkToAXSE";
+        return "AXSE";
     }
-          @Override
-          public String enter(Player p)  throws InterruptedException{
-          {
-                   String nextLocationName; 
-                   System.out.println("Finally here in AXSE *Sigh");
-                   Thread.sleep(1000);
-                   System.out.println("what time is it? " + p.getHour());
-                   Thread.sleep(1000);
-                   System.out.println("Hey Player are you going to class?");
-                   Thread.sleep(1000);
-                   System.out.println("Do you want to go to class? [Yes] or [No]");
-                   Thread.sleep(1000);
-                   Scanner scc = new Scanner(System.in);
-                   String input = scc.next();
-                   input = input.toUpperCase();
-                    if(input.equals("NO")){
-                       System.out.println("Um no I'm gonna leave right now.");
-                       Thread.sleep(1000);
-                       p.changeHour(p.getHour() - 1);
-                   }
-                   return "HouseWakeUp";
-                } 
-         
-    }
+    
+    @Override
+    public String enter(Player p)  throws InterruptedException{
+       
+         String nextLocationName; 
+         System.out.println("Finally here in AXSE *Sigh");
+         Thread.sleep(1000);
+         System.out.println("what time is it? " + p.getHour());
+         Thread.sleep(1000);
+         System.out.println("Hey Player are you going to class?");
+         Thread.sleep(1000);
+         System.out.println("Do you want to go to class? [Yes] or [No]");
+         Thread.sleep(1000);
+         Scanner scc = new Scanner(System.in);
+         String input = scc.next();
+         input = input.toUpperCase();
+         if(input.equals("NO")){
+          System.out.println("Um no I'm gonna leave right now.");
+          Thread.sleep(1000);
+          p.changeHour(p.getHour() - 1);
+         }
+         return "HouseWakeUp";
+    }  
 }
---
+++ StonyBrook.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Stony_Brook here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StonyBrook implements Location
+{
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    @Override
+    public String getName()
+    {
+        return "StonyBrook";
+    }
+    
+    @Override
+    public String enter(Player p) throws InterruptedException{
+      return "adwda";
+      
+    }
+}
