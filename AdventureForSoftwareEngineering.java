f2b60ac8b2108e1d5db50d0f494d73e44c353534
Ameer <abaksh@afsenyc.org>
2018-05-02 11:54:02 -0400

    more stony updates

--- AdventureForSoftwareEngineering.java
+++ AdventureForSoftwareEngineering.java
@@ -14,12 +14,12 @@ public class AdventureForSoftwareEngineering implements Game{
         List<Location> levels = new ArrayList<Location>();
         //levels.add(new HouseWakeUp());
         //levels.add(new Starbucks());
-        levels.add(new AXSE());
+        //levels.add(new AXSE());
         //levels.add(new AXSE());
         levels.add(new StonyBrook());
         //levels.add(new AXSE());
         //levels.add(new StonyBrook());
-        levels.add(new Harvard());
+        //levels.add(new Harvard());
         return levels;
     }
 
--- StonyBrook.java
+++ StonyBrook.java
@@ -15,7 +15,19 @@ public class StonyBrook implements Location
     
     @Override
     public String enter(Player p) throws InterruptedException{
-      print("*It's your first day at SUNY Stony Brook*", 1000);
+      System.out.print("a");
+      Thread.sleep(100);
+       System.out.print("a");
+      Thread.sleep(100);
+      System.out.print("a");
+      Thread.sleep(100);
+      System.out.print("a");
+      Thread.sleep(100);
+      System.out.print("a");
+      Thread.sleep(100);
+      System.out.print("a");
+      Thread.sleep(100);
+      //typingEffect("*It's your first day at SUNY Stony Brook*", 100);
       print("*Some random nerd walks up to you and introduces himself!*", 1500);
       print("'Uh, h... he... hey, I'm To... Toby' -Toby", 1500);
       print("*How are you going to respond?*", 1000);
@@ -40,6 +52,15 @@ public class StonyBrook implements Location
         print("Jesus christ, you are so mean!", 1100);
         print("'I... I.... I'mmm sorry" + " Player!' -Toby", 1000);
         print("'What class do you have right now?' -Toby", 1200);
+        print("*What are you going to do?*", 1200);
+        //print.text("[Lie] or [Truth]")
+        Scanner a = new Scanner(System.in);
+        String userrinput = a.next();
+        userrinput = userrinput.toUpperCase();
+        if(userrinput.equals("LIE")){
+          print.text("Oh I have some liberal art's class right now!", 1300);
+          print.text("'Alright, well I have Computer Science right now. cya!'", 1200);
+        }
       }
       else if(userinput.equals("LMAO")){
         print("Lmao? Do I know you?", 1000);
@@ -51,4 +72,11 @@ public class StonyBrook implements Location
         hiOrLmao();
       }
     }
+    
+    public void typingEffect(String a, int t) throws InterruptedException{
+      for(int i = 0; i < a.length() + 1; i++){
+        System.out.print(a.substring(i, i++));
+        Thread.sleep(t);
+      }
+    } 
 }
